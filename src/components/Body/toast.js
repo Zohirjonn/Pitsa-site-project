bb06a2468b89e2931ecfeaeec6081f8715e71ce0
Zohirjon <example@mail.com>
2023-03-27 12:11:17 +0500

    toasty.js

--- src/components/Body/toast.js
+++ src/components/Body/toast.js
@@ -1,9 +1,12 @@
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
 export function notify(name, type, size) {
-  return toast.success([name + " | ", "" + type, " " + size + "-см", " + 1"], {
-    position: toast.POSITION.TOP_RIGHT,
-    rtl: false,
-    autoClose: 1000,
-  });
+  return toast.success(
+    [name + " | ", "" + type, " " + size + "-см", " +1 шт."],
+    {
+      position: toast.POSITION.TOP_RIGHT,
+      rtl: false,
+      autoClose: 1000,
+    }
+  );
 }
